f2a7d30c8bbea659f6724cf351b1e026d3d5277b
trentpark8800 <47793696+trentpark8800@users.noreply.github.com>
2026-02-14 13:12:46 +0200

    Add initial implementation of simple notebook using Marimo framework

---
+++ simple_notebook.py
@@ -0,0 +1,28 @@
+import marimo
+
+__generated_with = "0.19.11"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return
+
+
+@app.cell
+def _(x):
+    y = 10
+    print(x+y)
+    return
+
+
+@app.cell
+def _():
+    x = 6
+    return (x,)
+
+
+if __name__ == "__main__":
+    app.run()
